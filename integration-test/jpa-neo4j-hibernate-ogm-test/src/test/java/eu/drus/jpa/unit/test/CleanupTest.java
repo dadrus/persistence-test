d3968af1b62dee067df903eaf34ca46acb8698dc
Dimitrij Drus <dadrus@gmx.de>
2017-09-12 03:04:43 +0200

    test enabled

--- integration-test/jpa-neo4j-hibernate-ogm-test/src/test/java/eu/drus/jpa/unit/test/CleanupTest.java
+++ integration-test/jpa-neo4j-hibernate-ogm-test/src/test/java/eu/drus/jpa/unit/test/CleanupTest.java
@@ -1,12 +1,10 @@
 package eu.drus.jpa.unit.test;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import eu.drus.jpa.unit.api.JpaUnitRunner;
 
 @RunWith(JpaUnitRunner.class)
-@Ignore
 public class CleanupTest extends AbstractCleanupTest {
     // the first test case runs already into NPE during transaction commit in hibernate OGM code
 }
