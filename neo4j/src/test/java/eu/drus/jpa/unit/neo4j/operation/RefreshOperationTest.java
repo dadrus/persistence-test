a247f5fe84243c95de8ea2947dc4e77a38978da9
Dimitrij Drus <dadrus@gmx.de>
2017-09-28 17:23:07 +0200

    tests for Neo4j operations implemented

---
+++ neo4j/src/test/java/eu/drus/jpa/unit/neo4j/operation/RefreshOperationTest.java
@@ -0,0 +1,76 @@
+package eu.drus.jpa.unit.neo4j.operation;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.ClassBasedEdgeFactory;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import eu.drus.jpa.unit.neo4j.dataset.Edge;
+import eu.drus.jpa.unit.neo4j.dataset.Node;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RefreshOperationTest {
+
+    @Mock
+    private Connection connection;
+
+    @Spy
+    private RefreshOperation operation;
+
+    @Before
+    public void prepareMocks() throws SQLException {
+        doAnswer(i -> null).when(operation).executeQuery(any(Connection.class), anyString());
+    }
+
+    @Test
+    public void testExecute() throws SQLException {
+        // GIVEN
+        final Graph<Node, Edge> graph = new DefaultDirectedGraph<>(new ClassBasedEdgeFactory<>(Edge.class));
+        final Map<String, Object> n1Attrs = new HashMap<>();
+        n1Attrs.put("id", new Long(1));
+        final Map<String, Object> n2Attrs = new HashMap<>();
+        n2Attrs.put("id", new Long(2));
+        final Map<String, Object> e1Attrs = new HashMap<>();
+        e1Attrs.put("id", new Long(3));
+        final Node n1 = new Node("n1", Arrays.asList("Node"), n1Attrs);
+        final Node n2 = new Node("n2", Arrays.asList("Node"), n2Attrs);
+        final Edge e1 = new Edge(n1, n2, "e1", Arrays.asList("Edge"), e1Attrs);
+        graph.addVertex(n1);
+        graph.addVertex(n2);
+        graph.addEdge(n1, n2, e1);
+
+        // WHEN
+        operation.execute(connection, graph);
+
+        // THEN
+        final ArgumentCaptor<String> queryCaptor = ArgumentCaptor.forClass(String.class);
+        verify(operation, times(2)).executeQuery(eq(connection), queryCaptor.capture());
+        final List<String> queries = queryCaptor.getAllValues();
+        final String query1 = queries.get(0);
+        final String query2 = queries.get(1);
+        assertThat(query1, containsString("MERGE (n1:Node {id:1}) SET n1.id=1"));
+        assertThat(query2, containsString("MERGE (n2:Node {id:2}) SET n2.id=2"));
+    }
+}
