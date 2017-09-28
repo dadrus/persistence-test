b3663971d816a5ae319ac3f5d51d88d74046c30f
Dimitrij Drus <dadrus@gmx.de>
2017-09-28 21:09:56 +0200

    added protocol specific verifications. No defaults any more

--- neo4j/src/main/java/eu/drus/jpa/unit/neo4j/ext/HibernateOgmConfiguration.java
+++ neo4j/src/main/java/eu/drus/jpa/unit/neo4j/ext/HibernateOgmConfiguration.java
@@ -13,10 +13,10 @@ public class HibernateOgmConfiguration implements Configuration {
     // hibernate.ogm.neo4j.configuration_resource_name
     // hibernate.schema_update.unique_constraint_strategy
 
-    private static final String HIBERNATE_OGM_DATASTORE_PASSWORD = "hibernate.ogm.datastore.password";
-    private static final String HIBERNATE_OGM_DATASTORE_USERNAME = "hibernate.ogm.datastore.username";
-    private static final String HIBERNATE_OGM_DATASTORE_HOST = "hibernate.ogm.datastore.host";
-    private static final String HIBERNATE_OGM_DATASTORE_PROVIDER = "hibernate.ogm.datastore.provider";
+    protected static final String HIBERNATE_OGM_DATASTORE_PASSWORD = "hibernate.ogm.datastore.password";
+    protected static final String HIBERNATE_OGM_DATASTORE_USERNAME = "hibernate.ogm.datastore.username";
+    protected static final String HIBERNATE_OGM_DATASTORE_HOST = "hibernate.ogm.datastore.host";
+    protected static final String HIBERNATE_OGM_DATASTORE_PROVIDER = "hibernate.ogm.datastore.provider";
 
     public static class ConfigurationFactoryImpl implements ConfigurationFactory {
 
@@ -49,7 +49,7 @@ public class HibernateOgmConfiguration implements Configuration {
         final String key = (String) properties.get(HIBERNATE_OGM_DATASTORE_PROVIDER);
         if (key.equals("neo4j_bolt")) {
             url = "jdbc:neo4j:bolt://" + host;
-        } else {
+        } else if (key.equals("neo4j_http")) {
             url = "jdbc:neo4j:http://" + host;
         }
     }
