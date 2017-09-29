1ed55ed9f2528c67cb702330bde254a31b12b98a
Dimitrij Drus <dadrus@gmx.de>
2017-09-29 09:18:05 +0200

    Tests for AttributeTypeConverter implemented

--- neo4j/src/main/java/eu/drus/jpa/unit/neo4j/dataset/AttributeTypeConverter.java
+++ neo4j/src/main/java/eu/drus/jpa/unit/neo4j/dataset/AttributeTypeConverter.java
@@ -12,16 +12,14 @@ public final class AttributeTypeConverter {
         switch (attribute.getType()) {
         case BOOLEAN:
             return Boolean.valueOf(attribute.getValue());
-        case DOUBLE:
-            return Double.valueOf(attribute.getValue());
         case FLOAT:
             // float types are always represented as double in neo4j
             // https://neo4j.com/docs/developer-manual/current/drivers/cypher-values/#driver-type-mappings
+        case DOUBLE:
             return Double.valueOf(attribute.getValue());
         case INT:
             // int types are always represented as long in neo4j
             // https://neo4j.com/docs/developer-manual/current/drivers/cypher-values/#driver-type-mappings
-            return Long.valueOf(attribute.getValue());
         case LONG:
             return Long.valueOf(attribute.getValue());
         case STRING:
