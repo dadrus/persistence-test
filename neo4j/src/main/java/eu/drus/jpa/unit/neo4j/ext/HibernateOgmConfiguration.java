29a61736c64e716cf9dc5f0b0abb212f71568a3a
Dimitrij Drus <dadrus@gmx.de>
2017-11-06 17:33:50 +0100

    added support for embedded mode for hibernate OGM.

--- neo4j/src/main/java/eu/drus/jpa/unit/neo4j/ext/HibernateOgmConfiguration.java
+++ neo4j/src/main/java/eu/drus/jpa/unit/neo4j/ext/HibernateOgmConfiguration.java
@@ -1,23 +1,18 @@
 package eu.drus.jpa.unit.neo4j.ext;
 
+import java.io.IOException;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
-import com.zaxxer.hikari.HikariDataSource;
-
+import eu.drus.jpa.unit.api.JpaUnitException;
 import eu.drus.jpa.unit.spi.PersistenceUnitDescriptor;
 
-public class HibernateOgmConfiguration implements Configuration {
-
-    // hibernate.ogm.neo4j.database_path
-    // hibernate.ogm.neo4j.configuration_resource_name
-    // hibernate.schema_update.unique_constraint_strategy
+public class HibernateOgmConfiguration extends AbstractConfiguration {
 
     protected static final String HIBERNATE_OGM_DATASTORE_PASSWORD = "hibernate.ogm.datastore.password";
     protected static final String HIBERNATE_OGM_DATASTORE_USERNAME = "hibernate.ogm.datastore.username";
     protected static final String HIBERNATE_OGM_DATASTORE_HOST = "hibernate.ogm.datastore.host";
     protected static final String HIBERNATE_OGM_DATASTORE_PROVIDER = "hibernate.ogm.datastore.provider";
+    protected static final String HIBERNATE_OGM_DATABASE_PATH = "hibernate.ogm.neo4j.database_path";
 
     public static class ConfigurationFactoryImpl implements ConfigurationFactory {
 
@@ -25,47 +20,39 @@ public class HibernateOgmConfiguration implements Configuration {
         public boolean isSupported(final PersistenceUnitDescriptor descriptor) {
             final Map<String, Object> dbConfig = descriptor.getProperties();
 
-            final String key = (String) dbConfig.get(HIBERNATE_OGM_DATASTORE_PROVIDER);
+            final String provider = (String) dbConfig.get(HIBERNATE_OGM_DATASTORE_PROVIDER);
 
-            return key != null && (key.equals("neo4j_bolt") || key.equals("neo4j_http"));
+            return provider != null
+                    && (provider.equals("neo4j_bolt") || provider.equals("neo4j_http") || provider.equals("neo4j_embedded"));
         }
 
         @Override
         public Configuration createConfiguration(final PersistenceUnitDescriptor descriptor) {
-            return new HibernateOgmConfiguration(descriptor);
+            try {
+                return new HibernateOgmConfiguration(descriptor);
+            } catch (final IOException e) {
+                throw new JpaUnitException("Could not create HibernateOgmConfiguration", e);
+            }
         }
     }
 
-    private String connectionUrl;
-    private String username;
-    private String password;
-
-    private HibernateOgmConfiguration(final PersistenceUnitDescriptor descriptor) {
+    private HibernateOgmConfiguration(final PersistenceUnitDescriptor descriptor) throws IOException {
         final Map<String, Object> properties = descriptor.getProperties();
 
         final String host = (String) properties.get(HIBERNATE_OGM_DATASTORE_HOST);
         username = (String) properties.get(HIBERNATE_OGM_DATASTORE_USERNAME);
         password = (String) properties.get(HIBERNATE_OGM_DATASTORE_PASSWORD);
 
-        final String key = (String) properties.get(HIBERNATE_OGM_DATASTORE_PROVIDER);
-        if (key.equals("neo4j_bolt")) {
+        final String provider = (String) properties.get(HIBERNATE_OGM_DATASTORE_PROVIDER);
+        if (provider.equals("neo4j_bolt")) {
             connectionUrl = "jdbc:neo4j:bolt://" + host;
-        } else if (key.equals("neo4j_http")) {
+        } else if (provider.equals("neo4j_http")) {
             connectionUrl = "jdbc:neo4j:http://" + host;
+        } else if (provider.equals("neo4j_embedded")) {
+            loadConfigurationForEmbeddedSetup();
+        } else {
+            throw new JpaUnitException("Unsupported provider configuration for Hibernate OGM: " + provider);
         }
-    }
-
-    @Override
-    public DataSource createDataSource() {
-        final HikariDataSource ds = new HikariDataSource();
-        ds.setDriverClassName("org.neo4j.jdbc.Driver");
-        ds.setUsername(username);
-        ds.setPassword(password);
-        ds.setJdbcUrl(connectionUrl);
-        ds.setMinimumIdle(1);
-        ds.setMinimumIdle(2);
-
-        return ds;
     }
 
 }
--- neo4j/src/test/java/eu/drus/jpa/unit/neo4j/ext/HibernateOgmConfigurationTest.java
+++ neo4j/src/test/java/eu/drus/jpa/unit/neo4j/ext/HibernateOgmConfigurationTest.java
@@ -10,14 +10,23 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
@@ -31,6 +40,9 @@ import eu.drus.jpa.unit.spi.PersistenceUnitDescriptor;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(HibernateOgmConfiguration.class)
 public class HibernateOgmConfigurationTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
     @Mock
     private PersistenceUnitDescriptor descriptor;
@@ -63,6 +75,21 @@ public class HibernateOgmConfigurationTest {
         // GIVEN
         final Map<String, Object> dbConfig = new HashMap<>();
         dbConfig.put(HibernateOgmConfiguration.HIBERNATE_OGM_DATASTORE_PROVIDER, "neo4j_http");
+        when(descriptor.getProperties()).thenReturn(dbConfig);
+        final ConfigurationFactory factory = new HibernateOgmConfiguration.ConfigurationFactoryImpl();
+
+        // WHEN
+        final boolean isSupported = factory.isSupported(descriptor);
+
+        // THEN
+        assertTrue(isSupported);
+    }
+
+    @Test
+    public void testNeo4JEmbeddedProtocolSupport() {
+        // GIVEN
+        final Map<String, Object> dbConfig = new HashMap<>();
+        dbConfig.put(HibernateOgmConfiguration.HIBERNATE_OGM_DATASTORE_PROVIDER, "neo4j_embedded");
         when(descriptor.getProperties()).thenReturn(dbConfig);
         final ConfigurationFactory factory = new HibernateOgmConfiguration.ConfigurationFactoryImpl();
 
@@ -166,4 +193,59 @@ public class HibernateOgmConfigurationTest {
         assertThat(userCaptor.getValue(), equalTo("neo4j"));
         assertThat(passCaptor.getValue(), equalTo("test"));
     }
+
+    @Test
+    public void testCreateEmbeddedDataSource() throws SQLException, IOException {
+        // GIVEN
+        final Map<String, Object> dbConfig = new HashMap<>();
+        dbConfig.put(HibernateOgmConfiguration.HIBERNATE_OGM_DATASTORE_PROVIDER, "neo4j_embedded");
+        dbConfig.put(HibernateOgmConfiguration.HIBERNATE_OGM_DATABASE_PATH, System.getProperty("java.io.tmpdir") + "/neo4j_test_db");
+        final File propsFile = temporaryFolder.newFile("jpa-unit.properties");
+        final Properties props = new Properties();
+        props.put("connection.url", "jdbc:neo4j:bolt://localhost:7687");
+        props.put("user.name", "neo4j");
+        props.put("user.password", "test");
+        try (OutputStream out = new FileOutputStream(propsFile)) {
+            props.store(out, "JPA Unit configuration used, when embedded provider is configured");
+        }
+
+        // make the file available through class loader
+        Thread.currentThread().setContextClassLoader(new ClassLoader() {
+            @Override
+            public URL getResource(final String name) {
+                if (name.equals("jpa-unit.properties")) {
+                    try {
+                        return propsFile.toURI().toURL();
+                    } catch (final MalformedURLException e) {
+                        throw new RuntimeException("could not load " + name);
+                    }
+                } else {
+                    return super.getResource(name);
+                }
+            }
+        });
+
+        when(descriptor.getProperties()).thenReturn(dbConfig);
+        final ConfigurationFactory factory = new HibernateOgmConfiguration.ConfigurationFactoryImpl();
+        final Configuration configuration = factory.createConfiguration(descriptor);
+
+        // WHEN
+        final DataSource ds = configuration.createDataSource();
+
+        // THEN
+        assertNotNull(ds);
+
+        final ArgumentCaptor<String> urlCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> userCaptor = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<String> passCaptor = ArgumentCaptor.forClass(String.class);
+        verify(dataSource).setDriverClassName(eq("org.neo4j.jdbc.Driver"));
+        verify(dataSource).setUsername(userCaptor.capture());
+        verify(dataSource).setPassword(passCaptor.capture());
+        verify(dataSource).setJdbcUrl(urlCaptor.capture());
+
+        assertThat(urlCaptor.getValue(), equalTo("jdbc:neo4j:bolt://localhost:7687"));
+        assertThat(userCaptor.getValue(), equalTo("neo4j"));
+        assertThat(passCaptor.getValue(), equalTo("test"));
+    }
+
 }
